e73747367f851c2ca82eba5c5664cf4551a1fd36
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-11-25 09:25:45 +0700

    Add routes for show all data & show data by id

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,20 @@
-const { addNoteHandler } = require('./handler');
+const {
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  addNoteHandler,
+} = require('./handler');
 
 const routes = [
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
   {
     method: 'POST',
     path: '/notes',
